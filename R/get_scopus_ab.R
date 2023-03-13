d5ff480212e30e1c996ce1cb2cfda137a3d4e545
darrennorris <doon75@hotmail.com>
2023-03-13 15:16:56 -0300

    update search and functions

--- R/get_scopus_ab.R
+++ R/get_scopus_ab.R
@@ -1,7 +1,7 @@
 get_scopus_ab <- function(x) {
   #get content from Scopus
   alt_res <-  safe_scopus_ab(id = x$id_pii, identifier = "pii",
-                             verbose = FALSE) 
+                             verbose = FALSE, headers = insttoken) 
   if(is.null(alt_res$error)){
     dfout <- data.frame(ab_scopus = alt_res$result$content$`abstracts-retrieval-response`$coredata$`dc:description`, 
                         doi_scopus = alt_res$result$content$`abstracts-retrieval-response`$coredata$`prism:doi`)
--- R/get_scopus_abdoi.R
+++ R/get_scopus_abdoi.R
@@ -1,10 +1,16 @@
 get_scopus_abdoi <- function(x) {
   #get content from Scopus
   alt_res <-  safe_scopus_ab(id = x$doi_clean, identifier = "doi",
-                             verbose = FALSE) 
+                             verbose = FALSE, headers = insttoken) 
   if(!is.null(alt_res$result)){
+    abres <- alt_res$result$content$`abstracts-retrieval-response`$coredata$`dc:description`
+    if(!is.null(abres)){
     dfout <- data.frame(ab_scopus = alt_res$result$content$`abstracts-retrieval-response`$coredata$`dc:description`, 
-                        doi_scopus = alt_res$result$content$`abstracts-retrieval-response`$coredata$`prism:doi`)
+                        doi_scopus = alt_res$result$content$`abstracts-retrieval-response`$coredata$`prism:doi`) 
+    }else{ 
+      dfout <- data.frame(ab_scopus = NA, 
+                               doi_scopus = x$doi_clean)
+      }
   }else{
     dfout <- data.frame(ab_scopus = NA, 
                         doi_scopus = NA)
